d0f26c0bd2fb63ade1b3e5181566e648810fa790
ksm <tnal7tndus@naver.com>
2024-03-07 17:28:59 +0900

    2024.03.07

---
+++ MyJavaScript/newJS04/m01_bar.js
@@ -0,0 +1,6 @@
+let menu='짜장';
+console.log(`** bar.js let menu=${menu}`);
+console.log(`** bar.js let menu=${window.menu}`); // undefined
+//var menu='짜장';
+// console.log(`** bar.js var menu=${menu}`);
+// console.log(`** bar.js var windeow.menu=${window.menu}`); // undefined
---
+++ MyJavaScript/newJS04/m01_foo.js
@@ -0,0 +1,6 @@
+let menu='짬뽕';
+console.log(`** foo.js: let menu=${menu}`);
+console.log(`** foo.js: let menu=${window.menu}`); // undefined
+//var menu='짬뽕';
+// console.log(`** foo.js: var menu=${menu}`);
+//console.log(`** foo.js: var windew.menu=${window.menu}`);
