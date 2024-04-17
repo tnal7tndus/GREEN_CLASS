d88fba2a708f20a0839c9ab40a7d5876ee2c389b
ksm <tnal7tndus@naver.com>
2024-04-17 18:04:15 +0900

    2024.04.17

---
+++ tomatofarm_Test/tomato/src/pages/admin/index/Index.jsx
@@ -0,0 +1,12 @@
+
+import Weather from './../weather/Weather';
+
+const Index = () => {
+    return (
+        <div id="weatherAndCalender">
+            <Weather />
+        </div>
+    )
+}
+
+export default Index;
